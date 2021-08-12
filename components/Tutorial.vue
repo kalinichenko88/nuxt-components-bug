f8d0152277b4334bba683b7d9212372a33578abf
Ivan Kalinichenko <ivan.kalinichenko@apliteni.com>
2021-08-12 18:18:29 +0300

    init

---
+++ components/Tutorial.vue
@@ -0,0 +1,12 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div style="border: 1px solid red">
+    Tutorial Component
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tutorial'
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <NuxtContent :document="page" />
+</template>
+
+<script>
+export default {
+  async asyncData(ctx) {
+    const page = await ctx.$content('hello').fetch()
+
+    return {
+      page
+    }
+  },
+  data() {
+    return {
+      page: undefined
+    }
+  }
+}
+</script>
